77f878769963ccd58ad5ec1de0a62c1f2c756de9
hochan222 <hochan222@hufs.ac.kr>
2020-03-08 21:42:18 +0000

    asds

--- precache-manifest.b9b43e892f3c33f20053fa299d89711e.js
+++ precache-manifest.b9b43e892f3c33f20053fa299d89711e.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "1b6335f5347a4c9cdb57",
-    "url": "/NASA-PICTURE-API/static/js/runtime~main.1b6335f5.js"
+    "url": "/static/js/runtime~main.1b6335f5.js"
   },
   {
     "revision": "3fa806ede76706689ea6",
-    "url": "/NASA-PICTURE-API/static/js/main.3fa806ed.chunk.js"
+    "url": "/static/js/main.3fa806ed.chunk.js"
   },
   {
     "revision": "e8673313f8e8ec66f325",
-    "url": "/NASA-PICTURE-API/static/js/1.e8673313.chunk.js"
+    "url": "/static/js/1.e8673313.chunk.js"
   },
   {
     "revision": "3fa806ede76706689ea6",
-    "url": "/NASA-PICTURE-API/static/css/main.2a382f7e.chunk.css"
+    "url": "/static/css/main.2a382f7e.chunk.css"
   },
   {
     "revision": "e8673313f8e8ec66f325",
-    "url": "/NASA-PICTURE-API/static/css/1.23c3e873.chunk.css"
+    "url": "/static/css/1.23c3e873.chunk.css"
   },
   {
     "revision": "dea8fd77e0faab0a62cfc6de7f1a2e64",
-    "url": "/NASA-PICTURE-API/index.html"
+    "url": "/index.html"
   }
 ];
